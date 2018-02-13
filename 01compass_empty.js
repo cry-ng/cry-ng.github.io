3cfa1ca451db2590a12a3146f6740ab0b77f9b9c
Cyrus Ng <CyrusNg@Cyruss-MacBook-Pro.local>
2018-02-13 15:05:10 -0600

    edit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,26 +8,25 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
          if (compassHeading > 315 || compassHeading < 45) {
-         text_area.textContent = "RISING SPINE";
+                text_area.textContent = "RISING SPINE";
         //calls function to alter content based on heading
        // myOrientation();
         }
         if (compassHeading > 45 || compassHeading < 135) {
-         text_area.textContent = "RETRACTING LEFT HAND";
+                text_area.textContent = "RETRACTING LEFT HAND";
         }
 
         if (compassHeading > 135 || compassHeading < 225) {
-         text_area.textContent = "SINKING FEET";
+                text_area.textContent = "SINKING FEET";
         }
 
         if (compassHeading > 225 || compassHeading < 315) {
-         text_area.textContent = "EXTENDING RIGHT HAND"
+                text_area.textContent = "EXTENDING RIGHT HAND";
         }
 // && is and and || is "or"
 
 //device orientation that can track orientation on space
 	window.addEventListener('deviceorientation', oriented, false);
-    
 
 
 //find the tag on the html page to put text into
