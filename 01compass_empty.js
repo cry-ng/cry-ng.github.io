1a6d86a22826e36169f1022f20e01567d9ae5c34
Cyrus Ng <CyrusNg@Cyruss-MacBook-Pro.local>
2018-02-13 14:57:07 -0600

    edit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,20 +8,20 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
          if (compassHeading > 315 || compassHeading < 45) {
-         text_area.textContent = "North";
+         text_area.textContent = "RISING SPINE";
         //calls function to alter content based on heading
        // myOrientation();
         }
         if (compassHeading > 45 || compassHeading < 135) {
-         text_area.textContent = "East";
+         text_area.textContent = "RETRACTING LEFT HAND";
         }
 
         if (compassHeading > 135 || compassHeading < 225) {
-         text_area.textContent = "South";
+         text_area.textContent = "SINKING FEET";
         }
 
         if (compassHeading > 225 || compassHeading < 315) {
-         text_area.textContent = "West"
+         text_area.textContent = "EXTENDING RIGHT HAND"
         }
 // && is and and || is "or"
 
